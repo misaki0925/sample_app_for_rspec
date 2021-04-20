08186beb9bb91e5b1c3b12ce896f3f04bfdbbbf7
hoshinomisaki <guangsakux@gmail.com>
2021-04-20 17:22:26 +0900

    02_モデルスペック

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+  email {"123@example.jp"}
+  password {"123456"}
+  password_confirmation{"123456"}
+  end
+end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid' do
+    task = FactoryBot.build(:task)
+    expect(task).to be_valid
+    expect(task.errors).to be_invalid  
+  end
+
+  it 'is invalid without title' do
+    task = FactoryBot.build(:task, title: nil)
+    expect(task).to be_invalid
+    expect(task.errors[:title]).to eq ["can't be blank"]
+  end
+
+  it 'is invalid with a duplicate title' do
+    FactoryBot.create(:task, title:"Test")
+    task = FactoryBot.build(:task, title: "Test")
+    expect(task).to be_invalid
+  end
+
+  it 'is valid another_title' do
+    FactoryBot.create(:task, title: "Test")
+    task = FactoryBot.build(:task, title:"Test_another")
+    expect(task).to be_valid
+  end
+
+  it 'is invalid without status' do
+    task = FactoryBot.build(:task, status: nil)
+    task.valid?
+    expect(task.errors[:status]).to include("can't be blank")  
+  end
 end
